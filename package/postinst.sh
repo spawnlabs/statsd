8b1a8f4f29e64db8d7538a37272aeb2f689ae66f
Chris Doughty <chrisdoughty@gamestop.com>
2012-11-15 10:05:13 -0600

    Fixing log/service ownership issues for upstart

--- package/postinst.sh
+++ package/postinst.sh
@@ -11,11 +11,16 @@ mkdir -p /var/log/statsd
 
 chown -R $USER:$GROUP /etc/statsd
 chown -R $USER:$GROUP /opt/statsd
+chown -R $USER:$GROUP /var/log/statsd
 
+# Setup some config files for execution time
 chmod 777 /opt/statsd/debian/scripts/start
 cp /opt/statsd/debian/statsd.upstart /etc/init/statsd.conf
 ln -fs /opt/statsd /usr/share/statsd
 ln -fs /opt/statsd/exampleConfig.js /etc/statsd/localConfig.js
 ln -fs /opt/statsd/debian/scripts /opt/statsd/scripts
+
+# Force our own user instead of using 'nobody'
+sed -i 's/nobody/statsd/' /opt/statsd/debian/scripts/start
 
 #DEBHELPER#
